2b9b4624c2156b93c41bc7b126980d923da1228c
dawidlehai <haidawidle@gmail.com>
2023-02-20 14:30:57 +0100

    feat: add hero title section

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -2,6 +2,9 @@ export interface Language {
   language: "en" | "pl";
 }
 
-export type Links = Map<string, string>;
+export type Links = {
+  text: string;
+  url: string;
+}[];
 
 export type Link = { text: string; url: string };
