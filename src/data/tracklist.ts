e354ef840189b10d428fedae832002e6f762571c
dawidlehai <haidawidle@gmail.com>
2023-02-24 20:18:25 +0100

    feat: add colors to root

---
+++ src/data/tracklist.ts
@@ -0,0 +1,41 @@
+export interface TracksType {
+  name: string;
+  genre: string;
+  length: string;
+  url: string;
+}
+
+export const tracklist: TracksType[] = [
+  {
+    name: "Hug",
+    genre: "Electronic",
+    length: "4:17",
+    url: "https://soundcloud.com/dawidlehai2/hug",
+  },
+  {
+    name: "Shinsaibashi",
+    genre: "Hip-Hop Beats",
+    length: "3:29",
+    url: "https://soundcloud.com/dawidlehai2/shinsaibashi",
+  },
+  {
+    name: "Adaptogen",
+    genre: "Dubstep",
+    length: "3:30",
+    url: "https://soundcloud.com/dawidlehai2/adaptogen",
+  },
+  {
+    name: "Utsettelse",
+    genre: "Drum & Bass",
+    length: "5:15",
+    url: "https://soundcloud.com/dawidlehai2/",
+  },
+  {
+    name: "Featured Tracks",
+    genre: "Various",
+    length: "7:13",
+    url: "https://soundcloud.com/dawidlehai2/featured-tracks",
+  },
+];
+
+export default tracklist;
