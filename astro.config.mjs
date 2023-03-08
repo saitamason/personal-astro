d9a5ff5772e0428bee6de785777067e3786e2bd0
dawidlehai <haidawidle@gmail.com>
2023-03-08 19:14:49 +0100

    feat: optimize figure images

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,10 @@ import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
 export default defineConfig({
   site: "https://dawidlehai.dev",
-  integrations: [react(), mdx()]
+  integrations: [react(), mdx(), image()]
 });
