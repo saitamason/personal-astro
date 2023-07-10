8afe42261d19ba037cc9dad5bf2ff92db0e10322
dawidlehai <92246829+dawidlehai@users.noreply.github.com>
2023-07-10 19:03:31 +0200

    feat: change soundcloud links

--- src/data/tracklist.ts
+++ src/data/tracklist.ts
@@ -10,31 +10,31 @@ export const tracklist: TrackType[] = [
     name: "Featured Tracks",
     genre: "Various",
     length: "7:13",
-    url: "https://soundcloud.com/dawidlehai2/featured-tracks",
+    url: "https://soundcloud.com/saitamason/featured-tracks",
   },
   {
     name: "Hug",
     genre: "Electronic",
     length: "4:17",
-    url: "https://soundcloud.com/dawidlehai2/hug",
+    url: "https://soundcloud.com/saitamason/hug",
   },
   {
     name: "Shinsaibashi",
     genre: "Hip-Hop Beats",
     length: "3:29",
-    url: "https://soundcloud.com/dawidlehai2/shinsaibashi",
+    url: "https://soundcloud.com/saitamason/shinsaibashi",
   },
   {
     name: "Adaptogen",
     genre: "Dubstep",
     length: "3:30",
-    url: "https://soundcloud.com/dawidlehai2/adaptogen",
+    url: "https://soundcloud.com/saitamason/adaptogen",
   },
   {
     name: "Utsettelse",
     genre: "Drum & Bass",
     length: "5:15",
-    url: "https://soundcloud.com/dawidlehai2/utsettelse",
+    url: "https://soundcloud.com/saitamason/utsettelse",
   },
 ];
 
